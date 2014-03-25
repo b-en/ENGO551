07555e00b09fabafcaa1dbcd412d606c1c8edbee
Ben Trodd <btrodd@gmail.com>
2014-03-24 18:30:41 -0600

    Added a simple theme to the map

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,17 @@
+
 $(document).ready(function() {
 
 var map;
+var theme = [{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#fffffa"}]},{"featureType":"water","stylers":[{"lightness":50}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"lightness":40}]}];
 
-function initialize() {
+function initialize () {
 	var myLatlng = new google.maps.LatLng(51.079529, -114.132446);
 	var myOptions = {
 		zoom: 2,
 		center: myLatlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		mapTypeControl: true
+		mapTypeControl: true,
+		styles: theme
 	};
 
 	map = new google.maps.Map(document.getElementById('mapCanvas'), myOptions);
